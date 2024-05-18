bb53897b3daaa1aa6df2fc4d128d01515719e705
Danny Fonseca <daniela.fonsecazumbado@gmail.com>
2024-05-17 20:46:42 -0600

    Added ej1.c to ej1 branch

--- ej1.c
+++ ej1.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Universidad de Costa Rica
+Programacion Bajo Plataformas Abiertas
+Laboratorio 4
+Elaborado por: Daniela Fonseca Zumbado B93070
+*/
+
+/*
+Bisqueda lineal
+-
+@n: numero ingresado por el usuario
+@array: arreglo ordenado definido en la funcion principal
+@f: tamaño del arreglo, se usa para saber la posicion del valor final en el arreglo
+*/
+
 int busqueda_lineal(int n, int array[], int size) {
     int i;
 
@@ -14,16 +29,56 @@ int busqueda_lineal(int n, int array[], int size) {
 }
 
 /*
-    @n = numero ingresado por el usuario
-    @array = array definido en la funcion principal
+Busqueda binaria
+-
+ @n: numero ingresado por el usuario
+ @array: arreglo ordenado definido en la funcion principal
+ @f: tamaño del arreglo, se usa para saber la posicion del valor final en el arreglo
 */
 
-int busqueda_binaria(int n, int array[], int size) {
-    int i;
+int busqueda_binaria(int n, int array[], int f) {
+    int i = 0;
+    int h;
+
+    while (i <= f) {
+        h = (i + f) / 2;
+        if (array[h] == n) {
+            return h; // Retorna h como la posicion del numero que se busca
+        }
+        else if (array[h] > n) {
+            f = h - 1;
+        }
+        else {
+            i = h + 1;
+        }
+    }
+    return -1;
 }
 
-int busqueda_lineal_recursiva() {
+/*
+Busqueda binaria recursiva
+-
+ @n: numero ingresado por el usuario
+ @array: arreglo ordenado definido en la funcion principal
+ @f: tamaño del arreglo, se usa para saber la posicion del valor final en el arreglo
+*/
 
+int busqueda_binaria_recursiva(int n, int array[], int i, int f) {
+    int h = (i + f) / 2;
+
+    if (i > f) {
+        return -1;
+    }
+
+    if (array[h] == n) {
+        return h; // Retorna h como la posicion del numero que se busca
+    }
+    else if (array[h] > n) {
+        return busqueda_binaria_recursiva(n, array, i, h - 1); // Se busca en la mitad izquierda
+    }
+    else {
+        return busqueda_binaria_recursiva(n, array, h + 1, f); // Se busca en la mitad derecha
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -33,10 +88,32 @@ int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
     int size = sizeof(array) / sizeof(array[0]); // Se calcula el tamaño del array
 
+    // Busqueda lineal
+
     int posicion_lineal = busqueda_lineal(n, array, size);
 
     if (posicion_lineal != -1) {
         printf("Busqueda lineal:\narray[%d] = %d\n", posicion_lineal, n);
+    } else {
+        printf("El numero %d no se encuentra en el array\n", n);
+    }
+
+    // Busqueda binaria
+
+    int posicion_binaria = busqueda_binaria(n, array, size);
+
+    if (posicion_binaria != -1) {
+        printf("Busqueda binaria:\narray[%d] = %d\n", posicion_binaria, n);
+    } else {
+        printf("El numero %d no se encuentra en el array\n", n);
+    }
+
+    // Busqueda binaria recursiva
+
+    int posicion_binaria_recursiva = busqueda_binaria_recursiva(n, array, 0, size);
+
+    if (posicion_binaria_recursiva != -1) {
+        printf("Busqueda binaria recursiva:\narray[%d] = %d\n", posicion_binaria_recursiva, n);
     } else {
         printf("El numero %d no se encuentra en el array\n", n);
     }
